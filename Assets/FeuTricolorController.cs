9e246326a81aa6f8ffe0523c36c6372dc7d23aa3
charlotte0702 <charlotte.barbe@ensc.fr>
2015-10-19 17:29:06 +0200

    PremierCommit

---
+++ Assets/FeuTricolorController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeuTricolorController : MonoBehaviour {
+
+
+    //GameObject gameObject; 
+    private Rigidbody rb; 
+    
+
+    // Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>(); 
+        GameObject FeuVert = transform.Find("SphereVerte").gameObject;
+        GameObject FeuOrange = transform.Find("SphereOrange").gameObject;
+        GameObject FeuRouge = transform.Find("SphereRouge").gameObject;
+
+        FeuVert.SetActive(true);
+        FeuOrange.SetActive(false);
+        FeuRouge.SetActive(false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
